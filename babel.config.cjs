a8fc49d9589c78cf69eda19aca4fb7fdefbaa4a1
HalfLife7 <d.nguyen@vehikl.com>
2023-08-02 16:43:30 -0400

    setup jest

---
+++ babel.config.cjs
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                targets: {
+                    node: 'current', // This will target the Node.js version used by Jest
+                },
+            },
+        ],
+        '@babel/preset-react', // Add this line to enable JSX transformations
+    ],
+};
---
+++ src/tests/App.test.jsx
@@ -0,0 +1,12 @@
+import App from "../App.jsx";
+import React from "react";
+import {render, screen} from "@testing-library/react";
+import '@testing-library/jest-dom'
+
+
+describe('App', () => {
+    it('renders', () => {
+        render(<App/>)
+        expect(screen.getByText('Hello World!')).toBeInTheDocument()
+    })
+})
