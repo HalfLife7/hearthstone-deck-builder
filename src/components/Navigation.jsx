04d90bb9d023ad5dfa6b63d75449b6cedea683a0
HalfLife7 <d.nguyen@vehikl.com>
2023-08-03 23:56:06 -0400

    style and create simple nav bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,13 @@
+import ShoppingCartIcon from "../icons/shopping-cart.jsx";
+import {Link} from "react-router-dom";
+
 const Navigation = () => {
     return (
-        <div>Navigation</div>
+        <div className="flex flex-row w-full h-14 bg-gray-200">
+            <Link className="h-14 flex items-center justify-center mx-4" to="/">Home</Link>
+            <Link className="h-14 flex items-center justify-center mx-4" to="/products">Products</Link>
+            <Link className="absolute right-0 top-0 w-14 h-14 flex items-center" to="/cart"><ShoppingCartIcon/></Link>
+        </div>
     )
 }
 
