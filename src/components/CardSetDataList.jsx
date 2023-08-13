4c50040b7b8e7040584d74ee4e1f9e5fe63b8b59
HalfLife7 <d.nguyen@vehikl.com>
2023-08-13 15:35:13 -0400

    extract datalist into reusable component

--- src/components/CardSetDataList.jsx
+++ src/components/CardSetDataList.jsx
@@ -1,71 +1,10 @@
 import React, {useEffect, useRef, useState} from 'react';
+import DataList from "./DataList.jsx";
 
 const CardSetDataList = ({items, onChange}) => {
-    const [inputValue, setInputValue] = useState('');
-    const [suggestions, setSuggestions] = useState([]);
-    const [showDropdown, setShowDropdown] = useState(false);
-    const inputRef = useRef(null);
-
-    useEffect(() => {
-        setSuggestions(items);
-    }, [items]);
-
-    const handleInputChange = (e) => {
-        const value = e.target.value;
-        setInputValue(value);
-        const filteredSuggestions = items.filter(item =>
-            item.replace(/_/g, " ").toLowerCase().includes(value.toLowerCase())
-        );
-        setSuggestions(filteredSuggestions);
-        setShowDropdown(true);
-    };
-
-    const handleInputBlur = () => {
-        setTimeout(() => {
-            if (!inputRef.current?.contains(document.activeElement)) {
-                setShowDropdown(false);
-            }
-        }, 200);
-    };
-
-    const visibleSuggestions = showDropdown ? suggestions : [];
-
-    const handleSuggestOnClick = (e) => {
-        setInputValue(e.target.innerHTML);
-        setShowDropdown(false);
-        setSuggestions([]);
-        onChange(e.target.innerHTML);
-    };
-
     return (
-        <div>
-            <label className="inline-block align-middle text-center w-24" htmlFor="card-set">Card Set:</label>
-            <input
-                id="card-set"
-                type="text"
-                value={inputValue}
-                onChange={handleInputChange}
-                onFocus={() => setShowDropdown(true)}
-                onBlur={handleInputBlur}
-                ref={inputRef}
-                className="border w-48 capitalize"
-                aria-label="Select or search for a card set"
-            />
-            {showDropdown && (
-                <ul className="absolute z-50 w-48 max-h-40 overflow-y-auto ml-24">
-                    {visibleSuggestions.map((item, index) => (
-                        <li
-                            className="capitalize px-2 py-1 bg-white border cursor-pointer hover:whitespace-normal"
-                            onClick={handleSuggestOnClick}
-                            key={index}
-                        >
-                            {item.split("_").join(" ")}
-                        </li>
-                    ))}
-                </ul>
-            )}
-        </div>
-    );
+        <DataList items={items} onChange={onChange} label={"Card Set"} />
+    )
 };
 
 export default CardSetDataList;
---
+++ src/components/DataList.jsx
@@ -0,0 +1,73 @@
+import React, {useEffect, useRef, useState} from "react";
+
+const DataList = ({items, onChange, label}) => {
+    const hyphenatedLabel = label.toLowerCase().replace(/ /g, '-')
+
+    const [inputValue, setInputValue] = useState('');
+    const [suggestions, setSuggestions] = useState([]);
+    const [showDropdown, setShowDropdown] = useState(false);
+    const inputRef = useRef(null);
+
+    useEffect(() => {
+        setSuggestions(items);
+    }, [items]);
+
+    const handleInputChange = (e) => {
+        const value = e.target.value;
+        setInputValue(value);
+        const filteredSuggestions = items.filter(item =>
+            item.replace(/_/g, " ").toLowerCase().includes(value.toLowerCase())
+        );
+        setSuggestions(filteredSuggestions);
+        setShowDropdown(true);
+    };
+
+    const handleInputBlur = () => {
+        setTimeout(() => {
+            if (!inputRef.current?.contains(document.activeElement)) {
+                setShowDropdown(false);
+            }
+        }, 200);
+    };
+
+    const visibleSuggestions = showDropdown ? suggestions : [];
+
+    const handleSuggestOnClick = (e) => {
+        setInputValue(e.target.innerHTML);
+        setShowDropdown(false);
+        setSuggestions([]);
+        onChange(e.target.innerHTML);
+    };
+
+    return (
+        <div>
+            <label className="inline-block align-middle text-center w-24" htmlFor={`${hyphenatedLabel}-data-list`}>{label}:</label>
+            <input
+                id={hyphenatedLabel}
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
+                onFocus={() => setShowDropdown(true)}
+                onBlur={handleInputBlur}
+                ref={inputRef}
+                className="border w-48 capitalize"
+                aria-label="Select or search for a card set"
+            />
+            {showDropdown && (
+                <ul className="absolute z-50 w-48 max-h-40 overflow-y-auto ml-24">
+                    {visibleSuggestions.map((item, index) => (
+                        <li
+                            className="capitalize px-2 py-1 bg-white border cursor-pointer hover:whitespace-normal"
+                            onClick={handleSuggestOnClick}
+                            key={index}
+                        >
+                            {item.split("_").join(" ")}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    )
+}
+
+export default DataList
